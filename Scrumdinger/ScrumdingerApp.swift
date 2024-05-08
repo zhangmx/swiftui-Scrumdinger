d11081a62f42e821d84c2036f2debcbf07983c00
mingxing <1308646+zhangmx@users.noreply.github.com>
2024-05-08 17:08:31 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by mingxing zhang on 2024/5/8.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
