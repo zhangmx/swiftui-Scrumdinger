13e8eb8c1990a036fc912fb74bec3f45bd4ab56d
mingxing <1308646+zhangmx@users.noreply.github.com>
2024-05-13 17:32:39 +0800

    creating-the-edit-view

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,58 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by mingxing zhang on 2024/5/11.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")){
+                TextField("Title", text:$scrum.title)
+                
+                HStack{
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30,step: 1){
+                        Text("Lenght")
+                    }
+                    .accessibilityLabel("\($scrum.lengthInMinutes) minutes")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                        .accessibilityHidden(true)
+                }
+                
+            }
+            Section(header: Text("Attendees")){
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete{ indexSet in
+                    scrum.attendees.remove(atOffsets: indexSet)
+                }
+                
+                HStack{
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation{
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }){
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
