6098d3e9c9133116f7668fd5259d13368ac7391a
mingxing <1308646+zhangmx@users.noreply.github.com>
2024-05-16 13:18:57 +0800

    passing-data-with-bindings

---
+++ Scrumdinger/ThemePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by mingxing zhang on 2024/5/16.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemePicker()
+}
---
+++ Scrumdinger/ThemeView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by mingxing zhang on 2024/5/16.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeView()
+}
